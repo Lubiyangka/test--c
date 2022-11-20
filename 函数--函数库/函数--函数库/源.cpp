8d4755f92f3dbca6ab369858614aadf8e92cb266
Lubiyangka <115919739+Lubiyangka@users.noreply.github.com>
2022-11-20 16:58:00 +0800

    c++的类练习

---
+++ 函数--函数库/函数--函数库/源.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+int main()
+{
+	int f[6] = { 1,2,3,4,5,6 };
+	int face;
+	for (int roll = 1; roll < 6000000; roll++) {
+		face = rand() % 6 + 1;
+		while (1) {
+			if(face==f[i])
+		}
+	}
+}
